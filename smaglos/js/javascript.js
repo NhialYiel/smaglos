6c877c1a89c022e34dd4204975e4aff6763a8819
NhialYiel <nhial98@hotmail.com>
2021-12-13 12:43:38 +0100

    5

--- smaglos/js/javascript.js
+++ smaglos/js/javascript.js
@@ -1,3 +1,16 @@
+const checkpoint = 300;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".arrow").style.opacity = opacity;
+});
+
+
 var slideIndex = 0;
 showSlides();
 
@@ -17,3 +30,20 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.menubar').addClass("sticky");
+      }else{
+          $('.menubar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  })});
